092bd51cbf3517386ae7f72e34f4e0dd76f589ce
Nessaj44 <johnedmoncoc@gmail.com>
2025-05-05 20:31:16 +0800

    Add home on navbar

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -6,19 +6,23 @@ function Navbar() {
   const { user, handleLogout } = useContext(AuthContext);
   
   return (
-      <nav className='flex justify-center bg-blue-800 font-semibold text-gray-200 gap-10 p-5'>
-        <NavLink to={"/UserList"} >Users</NavLink>
-        {user ? (
-          <>
-            <button onClick={handleLogout} className='cursor-pointer'>Logout</button>
-            <NavLink to="/UserPage">{user.username}</NavLink>
-          </>
-        ) : (
-          <>
-            <NavLink to="/Login">Login</NavLink>
-            <NavLink to={"/Signup"}>Signup</NavLink>        
-          </>
-        )}
+      <nav className='flex place-content-between bg-blue-800 font-semibold text-gray-200 gap-10 p-5'>
+        <NavLink to={"/"} className="ml-20">Message-board</NavLink>
+
+        <div className="flex justify-center gap-10 mr-20">
+          <NavLink to={"/UserList"} >Users</NavLink>
+          {user ? (
+            <>
+              <button onClick={handleLogout} className='cursor-pointer'>Logout</button>
+              <NavLink to="/UserPage">{user.username}</NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink to="/Login">Login</NavLink>
+              <NavLink to={"/Signup"}>Signup</NavLink>        
+            </>
+          )}
+        </div>
       </nav>
   )
 }
