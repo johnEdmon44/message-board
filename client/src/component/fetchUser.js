817da473121c3473842b8b079cca06c524e13381
Nessaj44 <johnedmoncoc@gmail.com>
2025-05-24 22:14:48 +0800

    Use env fetching users

--- client/src/component/fetchUser.js
+++ client/src/component/fetchUser.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchUser() {
   try {
-    const response = await axios.get("http://localhost:3400/user/user", { withCredentials: true });
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/user/user`, { withCredentials: true });
     console.log(response)
     return response.data.user;
   } catch {
