c2a3df7779e497caea771b1a5e829b105da3aa2a
Nessaj44 <johnedmoncoc@gmail.com>
2025-05-25 14:21:07 +0800

    Add secure cookie

--- server/app.js
+++ server/app.js
@@ -20,6 +20,11 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+    cookie: {
+    sameSite: "none",    // Required for cross-site cookies
+    secure: true,        // Required when using HTTPS (which Render uses)
+    httpOnly: true
+  }
 }));
 
 app.use(passport.initialize());
